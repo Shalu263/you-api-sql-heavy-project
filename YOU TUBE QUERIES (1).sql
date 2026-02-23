b2239fa40707ec2eb44d3b2556cadf8d48186754
SHALU KUMARI <shaluch019@gmail.com>
2026-02-23 21:48:05 +0530

    Add files via upload

---
+++ YOU TUBE QUERIES (1).sql
@@ -0,0 +1,12 @@
+SELECT * 
+FROM `you-tube-api-486713.youtube_list.Youtube_channel videos`;
+
+SELECT * 
+FROM `you-tube-api-486713.youtube_list.all youtube videos`;
+
+select * from `you-tube-api-486713.youtube_list.you tube channel data`;
+
+select * from `you-tube-api-486713.youtube_list.youtube channel list`;
+
+SELECT table_name
+FROM `you-tube-api-486713.youtube_list.INFORMATION_SCHEMA.TABLES`;
